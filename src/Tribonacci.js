bb2ee68b20aaebb7ae7e9493c5dfc0706dc9bf37
focus7eleven <441002295@qq.com>
2016-11-17 00:43:40 +0800

    :construction:

--- src/Tribonacci.js
+++ src/Tribonacci.js
@@ -2,10 +2,15 @@ function tribonacci(signature,n){
   //your code here
   let result = [];
   if(n<3){
-    result = signature.slice(0,n+1);
+    result = signature.slice(0,n);
   }else{
     result = result.concat(signature);
-    let snapshot = result;
-    let nextOne = snapshot.reduce((a,b)=>a+b,0);
+    let index = 0;
+    while(index < n-3){
+      let snapshot = result.slice(index,index+3);
+      let nextOne = snapshot.reduce((a,b)=>a+b,0);
+      result.push(nextOne);
+      index++;
+    }
   }
 }
