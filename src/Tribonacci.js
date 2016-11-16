27cde5bd4d88e74a6e2dd6dfd156731f9a1211e4
wuwenhuai <wwh@superid.cn>
2016-11-16 17:35:22 +0800

    :tada:

--- src/Tribonacci.js
+++ src/Tribonacci.js
@@ -1,5 +1,11 @@
 function tribonacci(signature,n){
   //your code here
+  let result = [];
+  if(n<3){
+    result = signature.slice(0,n+1);
+  }else{
+    result = result.concat(signature);
+    let snapshot = result;
+    let nextOne = snapshot.reduce((a,b)=>a+b,0);
+  }
 }
-
-module.exports = (a,b) => a+b;
